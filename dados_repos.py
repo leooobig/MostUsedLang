03307fb4b5479c731a7337d6bc04ebea32c6bff7
Leonardo Lima <leonardolimastos2003@gmail.com>
2025-02-12 15:43:47 -0300

    adicionado a função de criar arquivo com todas as informações requisitadas

---
+++ dados_repos.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+class DadosRepositorios:
+    def __init__(self, owner):
+        self.access_token = os.getenv("API_TOKEN")
+        self.owner = owner
+        self.headers = {'Authorization': 'Bearer ' + self.access_token,
+           'X-GitHub-Api-Version': '2022-11-28'}
+        self.api_url = 'https://api.github.com'
+    
+    repos_list = []
+
+    def listRepo(self):
+        page = 1
+        while True:
+            url_page = f'{self.api_url}/users/{self.owner}/repos?page={page}'
+            response = requests.get(url_page, headers=self.headers)
+            self.repos_list.append(response.json())
+            if len(response.json())==0:
+                break
+            page += 1
+
+    repos_name = []
+    repos_language = []
+
+    def nameRepo(self):
+        if len(self.repos_list) == 0:
+            self.listRepo()
+        for sub_page in self.repos_list:
+            for repo in sub_page:
+                self.repos_name.append(repo['name'])
+    
+    def languageRepo(self):
+        if len(self.repos_list) == 0:
+            self.listRepo()
+        for sub_page in self.repos_list:
+            for repo in sub_page:
+                self.repos_language.append(repo['language'])
+
+    def checkRepos(self):
+        checked = False
+        self.languageRepo()
+        self.nameRepo()
+        if self.repos_language and self.repos_name:
+            checked = True
+        return checked
+
+    data = None
+
+    def createData(self):
+        if self.checkRepos():
+            self.data = pd.DataFrame()
+            self.data['name_repository'] = self.repos_name
+            self.data['language_repository'] = self.repos_language
+        else:
+            print('Usuário não encontrado ou não existe repositórios para serem listados')
+
+    def createFile(self):
+        if isinstance(self.data, pd.DataFrame):
+            return self.data.to_csv(f'{self.owner}.csv')
+        else:
+            raise('O arquivo DataFrame não foi criado, tente chamar a função "createData" primeiro.')
+
+user = DadosRepositorios('leooobig')
+user.createData()
+user.createFile()
